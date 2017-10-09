ece2905c6eb068df15cbe61212c30d10c81714f4
Zak Lynch <zak.lynch@hotmail.com>
2017-10-09 22:01:35 +1100

    fixed file data
    fixed grabing the file data

--- Overlord.lua
+++ Overlord.lua
@@ -2,15 +2,18 @@ local discordia = require('discordia')
 local client = discordia.Client()
 
 file = io.open('botinfo.info','r')
-idAndToken=io.read()
-io.close()
-botToken, botId = idAndToken.split(',')
+print('reading file')
+idAndToken=file:read()
+print(idAndToken)
+file:close()
+
+botToken, botId = idAndToken:match("([^,]+),([^,]+)")
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
 client:on('messageCreate', function(message)
-	print('message sent ' .. message.type)
+	print('message sent ' .. message:type)
 	if message.content == '!ping' then
 		message.channel:send('Pong!')
 	end
