d74a135d72b98fb0a16508e59cc86020cf4e2dd3
Zak Lynch <zak.lynch@hotmail.com>
2017-10-09 21:25:16 +1100

    tokken and id
    added file that stores the bots tokken and id

--- Overlord.lua
+++ Overlord.lua
@@ -1,15 +1,19 @@
---require('lfs')
 local discordia = require('discordia')
 local client = discordia.Client()
 
+file = io.open('botinfo.info','r')
+idAndToken=io.read()
+io.close()
+botToken, botId = idAndToken.split(',')
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
 client:on('messageCreate', function(message)
+	print('message sent ' .. message.type)
 	if message.content == '!ping' then
 		message.channel:send('Pong!')
 	end
 end)
 
-client:run('Bot MzU5MTM1ODU5NjQ1MjE4ODE3.DLysUQ.6moJNVj4LWrJaOSDGJ55zHs0hm0')
+client:run('Bot ' .. botToken)
